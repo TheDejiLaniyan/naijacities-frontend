10cb88704b976614cfc5781527b602307e20c065
DejiLaniyan <adedeji.laniyan@gmail.com>
2022-10-13 21:27:51 +0100

    added build

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     // baseUrl: 'http://localhost:3500/naijacities',
-    baseUrl: 'https://naijacities.herokuapp.com/',
+    baseUrl: 'https://naijacities.herokuapp.com/naijacities',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
