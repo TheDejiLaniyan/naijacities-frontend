d4112d4a0a3de6c842e8c56c5a12718cec47e3cf
DejiLaniyan <adedeji.laniyan@gmail.com>
2022-10-14 10:43:14 +0100

    new

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import App from './App';
 import {Route, Routes, BrowserRouter} from 'react-router-dom' 
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
